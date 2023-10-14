145f1cf5695a0e308b96fcd5bb8165ba245ccc1a
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-10-14 17:17:16 -0300

    Remove Cart Item

--- js/main.js
+++ js/main.js
@@ -6,6 +6,29 @@ const closeCart = document.querySelector('#close-cart');
 cartIcon.onclick = () => {
   cart.classList.add('cart-active')
 }
+// Close Cart
 closeCart.onclick = () => {
   cart.classList.remove('cart-active')
+}
+
+// Add to Cart
+// A função é chamada quando tudo estiver carregado.
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
+  ready();
+}
+
+function ready() {
+  const removeCartButtons = document.getElementsByClassName('cart-remove');
+  for (let index = 0; index < removeCartButtons.length; index++) {
+    const button = removeCartButtons[index];
+    button.addEventListener('click', removeCartItem);
+  }
+}
+
+// Remove Cart Item
+function removeCartItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
 }
